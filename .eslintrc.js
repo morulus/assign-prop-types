a236c2aa0e3cf1e9c76895405d658f6ac514ea70
Vladimir Kalmykov <v.kalmykov@infotech.team>
2017-05-20 05:27:34 +0300

    eslint rules

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "env": {
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-undef": "off"
+    }
+};
